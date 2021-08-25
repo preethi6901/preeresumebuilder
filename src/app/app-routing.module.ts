9ab90d793f925c633f8de6b8003ae866de0337cc
Divine <dchelladurai@vmware.com>
2021-08-25 12:25:01 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Resume1Component } from './resume1/resume1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : "resume1",component : Resume1Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { Resume1Component } from './resume1/resume1.component';
+import {MatIconModule} from '@angular/material/icon'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Resume1Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
